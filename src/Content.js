be401f6ab1827563f4a5cfa0876ba4f0ea16b0cc
Abdalrahman_Amr <213835079+Abdalrahman-Amr-Dev@users.noreply.github.com>
2026-08-14 18:01:33 +0300

    Update package.json and package-lock.json to add cross-env as a dev dependency; refactor Content component to remove unused state and effects

--- src/Content.js
+++ src/Content.js
@@ -1,44 +1,25 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 
 const Content = () => {
-  const [name, setName] = useState("Dave");
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsweetened",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: true,
+      item: "Item 3",
+    },
+  ]);
 
-  const buttonRef = useRef(null);
-  useEffect(() => {
-    console.log("here");
-    console.log(buttonRef);
-  }, [count]);
-
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-    buttonRef.current.style.backgroundColor = "red";
-    buttonRef.current.style.borderColor = "red";
-  };
-
-  const handleClick = () => {
-    console.log("you clicked it ");
-    setCount(count + 1);
-  };
-  const handleClick2 = (name) => {
-    console.log(`you clicked it ${name}`);
-  };
-  const handleClick3 = (e) => {
-    console.log(e.target);
-  };
-
-  return (
-    <main>
-      <p onDoubleClick={() => handleClick()}>Hello {name}!</p>
-      <button ref={buttonRef} className="here1" onClick={handleNameChange}>
-        Change Name
-      </button>
-      <button onClick={() => handleClick2("Test")}>Click me 2</button>
-      <button onClick={(e) => handleClick3(e)}> Click me 3</button>
-    </main>
-  );
+  return <main></main>;
 };
 
 export default Content;
