f0099985d4fc3526dee64c50d90d5c21a968ec4e
Abdalrahman Amr <213835079+Abdalrahman-Amr-Dev@users.noreply.github.com>
2026-08-22 12:00:36 +0300

    Add shopping list rendering with check and delete handlers

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
   const [items, setItems] = useState([
     {
       id: 1,
       checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsweetened",
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
     },
     {
       id: 2,
@@ -14,12 +15,58 @@ const Content = () => {
     },
     {
       id: 3,
-      checked: true,
+      checked: false,
       item: "Item 3",
     },
   ]);
 
-  return <main></main>;
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item,
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  return (
+    <main>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li
+              className="item"
+              key={item.id}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+      )}
+    </main>
+  );
 };
 
 export default Content;
